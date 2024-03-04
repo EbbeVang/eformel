8c4d7a7febdd102028cedac459a0c38e640d38e9
Ebbe Vang <ev@evang.dk>
2024-03-04 10:42:06 +0100

    adds changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Formula from './components/Formula';
+import { MathJax, MathJaxContext } from 'better-react-mathjax';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MathJaxContext>
+        <h1>eformel.dk</h1>
+          <p>
+            <MathJax> {"\\((\\frac{10}{4x} \\approx 2^{12})\\)"} </MathJax>
+          </p>
+        </MathJaxContext>
       </header>
     </div>
   );
---
+++ src/components/Formula.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+interface FormulaProps {
+    
+}
+ 
+interface FormulaState {
+    
+}
+ 
+class Formula extends React.Component<FormulaProps, FormulaState> {
+    //state = {tex : string};
+    
+    render(){
+        return('hej matematik');
+    }; 
+    
+};
+ 
+export default Formula;
